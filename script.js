0106ae884a2e578e1ccdbff3d00b523ae9193c74
Skalik2 <106670239+Skalik2@users.noreply.github.com>
2023-08-19 00:15:40 +0200

    local files update projects/instagram/footer added

---
+++ script.js
@@ -0,0 +1,22 @@
+// masonry
+const masonry = new Macy({
+    container: '.masonry-div',
+    columns: 3,
+    margin: {
+        x: 30,
+        y: 40,
+    },
+    breakAt: {
+        760: {
+          margin: {
+            x: 5,
+            y: 5,
+          },
+          columns: 2
+        }
+    }
+});
+
+
+// more projects content lock
+
